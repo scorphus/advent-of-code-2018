e680e334e00a69cde9f1db9db68914d4ba7d909d
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2020-09-16 23:41:57 +0200

    day01: Write some tests

--- src/day01.rs
+++ src/day01.rs
@@ -30,6 +30,7 @@ impl Day {
         let mut freq = 0;
         let mut it = self.changes.iter().cycle();
         let mut seen_freqs = HashSet::new();
+        seen_freqs.insert(freq);
         loop {
             let change = it.next().expect("cyclic iterator exhausted (WTH?)");
             freq += change;
@@ -47,4 +48,32 @@ pub fn part01() -> i32 {
 
 pub fn part02() -> i32 {
     Day::read().part02()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! test_day {
+        ($($name:ident: $values:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (method, changes, expected) = $values;
+                    assert_eq!(expected, method(&Day{ changes }));
+                }
+            )*
+        }
+    }
+
+    test_day! {
+        test_part01_01: (Day::part01, vec![1, -2, 3, 1], 3),
+        test_part01_02: (Day::part01, vec![1, 1, 1], 3),
+        test_part01_03: (Day::part01, vec![1, 1, -2], 0),
+        test_part01_04: (Day::part01, vec![-1, -2, -3], -6),
+        test_part02_01: (Day::part02, vec![1, -1], 0),
+        test_part02_02: (Day::part02, vec![3, 3, 4, -2, -4], 10),
+        test_part02_03: (Day::part02, vec![-6, 3, 8, 5, -6], 5),
+        test_part02_04: (Day::part02, vec![7, 7, -2, -7, -4], 14),
+    }
 }
