bdb038a6716a719bbfaace5362fba1892188a59a
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2020-09-17 23:35:28 +0200

    Copy answer to clipboard

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate clipboard;
+
+use clipboard::ClipboardContext;
+use clipboard::ClipboardProvider;
+
 use std::env;
 
 mod day01;
@@ -10,14 +15,19 @@ fn main() {
 }
 
 fn solve_day(day_part: &str) {
-    println!(
-        "Your answer is: {}",
-        match day_part {
-            "1.1" => day01::part01().to_string(),
-            "1.2" => day01::part02().to_string(),
-            "2.1" => day02::part01().to_string(),
-            "2.2" => day02::part02(),
-            _ => "🤷".to_string(),
-        }
-    )
+    let answer = match day_part {
+        "1.1" => day01::part01().to_string(),
+        "1.2" => day01::part02().to_string(),
+        "2.1" => day02::part01().to_string(),
+        "2.2" => day02::part02(),
+        _ => "🤷".to_string(),
+    };
+    copy_to_clipboard(&answer);
+    println!("Your answer is: {} (already copied to clipboard)", answer)
+}
+
+fn copy_to_clipboard(content: &str) {
+    let mut ctx: ClipboardContext = ClipboardProvider::new().expect("no clipboard");
+    ctx.set_contents(content.to_string())
+        .expect("failed copying to clipboard");
 }
