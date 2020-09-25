fa8d7a70136cdea865536ba7111c6c2c915c3089
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2020-09-25 11:42:47 +0200

    Fix hook entries

--- src/input.rs
+++ src/input.rs
@@ -1,7 +1,11 @@
 use std::io::{self, BufRead};
 
 pub fn read_lines() -> Vec<String> {
-    io::stdin().lock().lines().map(|l| l.expect("read error")).collect()
+    io::stdin()
+        .lock()
+        .lines()
+        .map(|l| l.expect("read error"))
+        .collect()
 }
 
 pub fn string_to_i32(string: &str) -> i32 {
