1b60836cc4b30f42e70c760e18533f596231b48d
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2022-08-10 21:47:46 +0200

    src/day14: Add solution to Part One

--- src/day14.rs
+++ src/day14.rs
@@ -31,9 +31,20 @@ impl Day {
         }
     }
 
-    fn part01(&self) -> Vec<usize> {
-        println!("{:?}", self.recipes);
-        vec![3, 7]
+    fn part01(&mut self) -> Vec<usize> {
+        let mut recipes = vec![3, 7];
+        let target_len = self.recipes + 10;
+        let (mut elf1, mut elf2) = (0, 1);
+        while recipes.len() < target_len {
+            let new_recipe = recipes[elf1] + recipes[elf2];
+            if new_recipe >= 10 {
+                recipes.push(new_recipe / 10);
+            }
+            recipes.push(new_recipe % 10);
+            elf1 = (elf1 + 1 + recipes[elf1]) % recipes.len();
+            elf2 = (elf2 + 1 + recipes[elf2]) % recipes.len();
+        }
+        recipes[(target_len - 10)..target_len].to_vec()
     }
 
     fn part02(&self) -> Vec<usize> {
@@ -59,7 +70,10 @@ mod tests {
     }
 
     test_parts! {
-        test_part01_01: (part01, vec!["37"], "37"),
+        test_part01_01: (part01, vec!["9"], "5158916779"),
+        test_part01_02: (part01, vec!["5"], "0124515891"),
+        test_part01_03: (part01, vec!["18"], "9251071085"),
+        test_part01_04: (part01, vec!["2018"], "5941429882"),
         test_part02_01: (part02, vec!["37"], "37"),
     }
 }
