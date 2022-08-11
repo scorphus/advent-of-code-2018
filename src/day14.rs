0907b305156375dc02acef239278742d8d8d83ac
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2022-08-11 20:47:04 +0200

    src/day14: Add solution to Part Two

--- src/day14.rs
+++ src/day14.rs
@@ -10,32 +10,34 @@ pub fn part01<T: AsRef<str>>(lines: &[T]) -> String {
         .join("")
 }
 
-pub fn part02<T: AsRef<str>>(lines: &[T]) -> String {
-    Day::read_from(lines)
-        .part02()
-        .iter()
-        .map(|&id| id.to_string())
-        .collect::<Vec<String>>()
-        .join("")
+pub fn part02<T: AsRef<str>>(lines: &[T]) -> usize {
+    Day::read_from(lines).part02()
 }
 
 #[derive(Debug, Default)]
 struct Day {
-    recipes: usize,
+    recipes: Vec<usize>,
+    target_len: usize,
+    matched: usize,
 }
 
 impl Day {
     fn read_from<T: AsRef<str>>(lines: &[T]) -> Self {
+        let first_line = lines.first().expect("❌").as_ref();
         Day {
-            recipes: lines.first().expect("❌").as_ref().parse().expect("❌"),
+            recipes: first_line
+                .chars()
+                .map(|c| c.to_digit(10).expect("❌") as usize)
+                .collect(),
+            target_len: first_line.parse::<usize>().expect("❌") + 10,
+            matched: 0,
         }
     }
 
     fn part01(&mut self) -> Vec<usize> {
         let mut recipes = vec![3, 7];
-        let target_len = self.recipes + 10;
         let (mut elf1, mut elf2) = (0, 1);
-        while recipes.len() < target_len {
+        while recipes.len() < self.target_len {
             let new_recipe = recipes[elf1] + recipes[elf2];
             if new_recipe >= 10 {
                 recipes.push(new_recipe / 10);
@@ -44,12 +46,40 @@ impl Day {
             elf1 = (elf1 + 1 + recipes[elf1]) % recipes.len();
             elf2 = (elf2 + 1 + recipes[elf2]) % recipes.len();
         }
-        recipes[(target_len - 10)..target_len].to_vec()
+        recipes[(self.target_len - 10)..self.target_len].to_vec()
     }
 
-    fn part02(&self) -> Vec<usize> {
-        println!("{:?}", self.recipes);
-        vec![3, 7]
+    fn part02(&mut self) -> usize {
+        let mut recipes = vec![3, 7];
+        let (mut elf1, mut elf2) = (0, 1);
+        loop {
+            let new_recipe = recipes[elf1] + recipes[elf2];
+            if new_recipe >= 10 {
+                recipes.push(new_recipe / 10);
+                if self.has_match(new_recipe / 10) {
+                    break;
+                }
+            }
+            recipes.push(new_recipe % 10);
+            if self.has_match(new_recipe % 10) {
+                break;
+            }
+            elf1 = (elf1 + 1 + recipes[elf1]) % recipes.len();
+            elf2 = (elf2 + 1 + recipes[elf2]) % recipes.len();
+        }
+        recipes.len() - self.recipes.len()
+    }
+
+    fn has_match(&mut self, recipe: usize) -> bool {
+        if self.matched < self.recipes.len() && self.recipes[self.matched] == recipe {
+            self.matched += 1;
+            return self.matched == self.recipes.len();
+        }
+        self.matched = 0;
+        if self.recipes[0] == recipe {
+            self.matched = 1;
+        }
+        false
     }
 }
 
@@ -74,6 +104,9 @@ mod tests {
         test_part01_02: (part01, vec!["5"], "0124515891"),
         test_part01_03: (part01, vec!["18"], "9251071085"),
         test_part01_04: (part01, vec!["2018"], "5941429882"),
-        test_part02_01: (part02, vec!["37"], "37"),
+        test_part02_01: (part02, vec!["51589"], 9),
+        test_part02_02: (part02, vec!["01245"], 5),
+        test_part02_03: (part02, vec!["92510"], 18),
+        test_part02_04: (part02, vec!["59414"], 2018),
     }
 }
