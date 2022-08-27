06a29bd2fe1768e419d113d12af9ec7e2d1974cd
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2022-08-27 10:02:19 +0200

    src/day17: Add solution to Part One

--- src/day17.rs
+++ src/day17.rs
@@ -3,7 +3,7 @@ extern crate text_io;
 
 use text_io::scan;
 
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 pub fn part01<T: AsRef<str>>(lines: &[T]) -> usize {
     Day::read_from(lines).part01()
@@ -15,7 +15,7 @@ pub fn part02<T: AsRef<str>>(lines: &[T]) -> usize {
 
 #[derive(Debug, Default)]
 struct Day {
-    clay: HashSet<Location>,
+    ground: HashMap<Location, char>,
     min_y: usize,
     max_y: usize,
 }
@@ -35,12 +35,12 @@ impl Day {
             if xy == "x" {
                 day.set_min_max(v1, v1);
                 for v in v2..=v3 {
-                    day.clay.insert((v, v1));
+                    day.ground.insert((v, v1), '#');
                 }
             } else {
                 day.set_min_max(v2, v3);
                 for v in v2..=v3 {
-                    day.clay.insert((v1, v));
+                    day.ground.insert((v1, v), '#');
                 }
             }
         }
@@ -52,12 +52,56 @@ impl Day {
         self.max_y = std::cmp::max(self.max_y, y1);
     }
 
-    fn part01(&self) -> usize {
-        0
+    fn part01(&mut self) -> usize {
+        self.drain(500, 0);
+        self.ground
+            .iter()
+            .filter(|(&(_, y), &c)| self.min_y <= y && y <= self.max_y && (c == '~' || c == '|'))
+            .count()
     }
 
     fn part02(&self) -> usize {
         0
+    }
+
+    fn drain(&mut self, x: usize, y: usize) {
+        if y > self.max_y || self.is_blocked(x, y) {
+            return;
+        }
+        if self.is_sand(x, y) {
+            self.ground.insert((x, y), '|');
+            self.drain(x, y + 1);
+        }
+        if self.is_blocked(x, y + 1) {
+            let mut xl = x;
+            while self.is_blocked(xl, y + 1) && !self.is_blocked(xl, y) {
+                self.ground.insert((xl, y), '|');
+                xl -= 1;
+            }
+            let mut xr = x + 1;
+            while self.is_blocked(xr, y + 1) && !self.is_blocked(xr, y) {
+                self.ground.insert((xr, y), '|');
+                xr += 1;
+            }
+            if self.is_blocked(xl, y + 1) && self.is_blocked(xr, y + 1) {
+                for x in xl + 1..xr {
+                    self.ground.insert((x, y), '~');
+                }
+            } else {
+                self.drain(xl, y);
+                self.drain(xr, y);
+            }
+        }
+    }
+
+    fn is_blocked(&self, x: usize, y: usize) -> bool {
+        self.ground
+            .get(&(x, y))
+            .map_or(false, |c| *c == '#' || *c == '~')
+    }
+
+    fn is_sand(&self, x: usize, y: usize) -> bool {
+        !self.ground.contains_key(&(x, y))
     }
 }
 
@@ -87,7 +131,7 @@ mod tests {
             "x=498, y=10..13",
             "x=504, y=10..13",
             "y=13, x=498..504",
-        ], 0),
+        ], 57),
         test_part02_01: (part02, vec![
             "x=495, y=2..7",
             "y=7, x=495..501",
